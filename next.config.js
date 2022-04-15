bc07d8d78a32d5921b160ed3f674ad852354d4b0
dredshep <dredshep@gmail.com>
2022-04-15 17:40:59 -0400

    redirects now work for all current app pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /**
+  /cover
+  /terms
+  /bond
+  /stake
+  /farms
+  /govern
+  /archive
+  */
   redirects: () => [
     {
       source: '/cover',
       destination: 'https://app.solace.fi/cover',
+      permanent: true,
+    },
+    {
+      source: '/terms',
+      destination: 'https://app.solace.fi/terms',
+      permanent: true,
+    },
+    {
+      source: '/bond',
+      destination: 'https://app.solace.fi/bond',
+      permanent: true,
+    },
+    {
+      source: '/stake',
+      destination: 'https://app.solace.fi/stake',
+      permanent: true,
+    },
+    {
+      source: '/farms',
+      destination: 'https://app.solace.fi/farms',
+      permanent: true,
+    },
+    {
+      source: '/govern',
+      destination: 'https://app.solace.fi/govern',
+      permanent: true,
+    },
+    {
+      source: '/archive',
+      destination: 'https://app.solace.fi/archive',
       permanent: true,
     },
   ],
